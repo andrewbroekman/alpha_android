846bd3ae0427da7a5c37c28e4f248d2a7f820252
RuanKlinkert <u14022282@tuks.co.za>
2016-03-30 17:56:29 +0200

    Added view and edit buttons to items in listview

--- app/src/main/java/com/codinginfinity/android/ViewPublicationsActivity.java
+++ app/src/main/java/com/codinginfinity/android/ViewPublicationsActivity.java
@@ -1,10 +1,12 @@
 package com.codinginfinity.android;
 //Ruan Klinkert - 14022282
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,12 +16,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ViewPublicationsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -77,8 +85,9 @@ public class ViewPublicationsActivity extends AppCompatActivity
         items=new String[]{"Canada","China","Japan","USA","South-Africa"};
       //listItems.add("");
         listItems=new ArrayList<>(Arrays.asList(items));
-        adapter=new ArrayAdapter<String>(this,R.layout.list_item_view_publications, R.id.list_view_item_name_view_publication, listItems);
-        listView.setAdapter(adapter);
+        //adapter=new ArrayAdapter<String>(this,R.layout.list_item_view_publications, R.id.list_view_item_name_view_publication, listItems);
+        //listView.setAdapter(adapter);
+        listView.setAdapter(new MyListAdapter(this, R.layout.list_item_view_publications, listItems));
     }
 
     public void searchItem(String textToSearch){
@@ -139,5 +148,51 @@ public class ViewPublicationsActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public class MyListAdapter extends ArrayAdapter<String>{
+        public int layout;
+        public MyListAdapter(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            layout = resource;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder mainViewHolder = null;
+            if(convertView == null){
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewholder = new ViewHolder();
+                viewholder.publication_name = (TextView)convertView.findViewById(R.id.list_view_item_name_view_publication);
+                viewholder.view_btn = (ImageButton)convertView.findViewById(R.id.list_view_item_view_view_publication);
+                viewholder.edit_btn = (ImageButton)convertView.findViewById(R.id.list_view_item_edit_view_publication);
+                viewholder.view_btn.setOnClickListener(new View.OnClickListener(){
+                    //View button clicked
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getContext(), "View button was clicked for item " + position, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                viewholder.edit_btn.setOnClickListener(new View.OnClickListener(){
+                    //View button clicked
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getContext(), "Edit button was clicked for item " + position, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                convertView.setTag(viewholder);
+            }
+            else {
+                mainViewHolder = (ViewHolder) convertView.getTag();
+                mainViewHolder.publication_name.setText(getItem(position));
+            }
+            return super.getView(position, convertView, parent);
+        }
+    }
+    public class ViewHolder{
+        TextView publication_name;
+        ImageButton view_btn;
+        ImageButton edit_btn;
     }
 }
