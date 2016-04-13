b3c22a0b6597c6e066415d406f813b968582db2b
RuanKlinkert <u14022282@tuks.co.za>
2016-04-13 18:20:13 +0200

    Added load function

--- app/src/main/java/com/codinginfinity/android/LoginActivity.java
+++ app/src/main/java/com/codinginfinity/android/LoginActivity.java
@@ -9,10 +9,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class LoginActivity extends AppCompatActivity {
     public String path = Environment.getExternalStorageDirectory().getAbsolutePath();
@@ -209,6 +212,50 @@ public class LoginActivity extends AppCompatActivity {
             }
             catch (IOException e) {e.printStackTrace();}
         }
+    }
+
+    public static String[] Load(File file)
+    {
+        FileInputStream fis = null;
+        try
+        {
+            fis = new FileInputStream(file);
+        }
+        catch (FileNotFoundException e) {e.printStackTrace();}
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        String test;
+        int anzahl=0;
+        try
+        {
+            while ((test=br.readLine()) != null)
+            {
+                anzahl++;
+            }
+        }
+        catch (IOException e) {e.printStackTrace();}
+
+        try
+        {
+            fis.getChannel().position(0);
+        }
+        catch (IOException e) {e.printStackTrace();}
+
+        String[] array = new String[anzahl];
+
+        String line;
+        int i = 0;
+        try
+        {
+            while((line=br.readLine())!=null)
+            {
+                array[i] = line;
+                i++;
+            }
+        }
+        catch (IOException e) {e.printStackTrace();}
+        return array;
     }
 
 }
