67b16f071039541cf2550b3caa14f6c1ff00b055
MarkKlingenberg <u14020272@tuks.co.za>
2016-04-14 18:27:56 +0200

    Generate the groups from a json

--- app/src/main/java/com/codinginfinity/android/GroupList.java
+++ app/src/main/java/com/codinginfinity/android/GroupList.java
@@ -3,6 +3,7 @@ package com.codinginfinity.android;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -28,6 +29,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -54,7 +62,9 @@ import java.util.Locale;
 public class GroupList extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    public String path = Environment.getExternalStorageDirectory().getAbsolutePath();
     //Temporary until I know what the object returned from the server looks like
+
     public class Group{
         String name;
         String fieldOfStudy;
@@ -135,9 +145,45 @@ public class GroupList extends AppCompatActivity
      * @return nothing
      */
     public void initList(){
+        File file = new File(path + "/groups.json");
+        if (!file.exists()) { // Generate fake json
+            String groupsJsonString = "[" +
+                    "{" +
+                    "\"name\" : \"CIRG\"," +
+                    "\"fieldOfStudy\" : \"Artifical Inteligence\"," +
+                    "\"leader\" : \"Kimi.Raikkonen@gmail.com\"" +
+                    "}," +
+                    "{" +
+                    "\"name\" : \"ICSA\"," +
+                    "\"fieldOfStudy\" : \"Distributed Systems Security and Privacy\"," +
+                    "\"leader\" : \"Felipe.Massa@gmail.com\"" +
+                    "}," +
+                    "{" +
+                    "\"name\" : \"SSFM\"," +
+                    "\"fieldOfStudy\" : \"System Specifications and Formal Methods\"," +
+                    "\"leader\" : \"Fernando.Alonso@gmail.com\"" +
+                    "}," +
+                    "{" +
+                    "\"name\" : \"SESAr\"," +
+                    "\"fieldOfStudy\" : \"Software Engineering and Software Architecture\"," +
+                    "\"leader\" : \"Jenson.Button@gmail.com\"" +
+                    "}," +
+                    "{" +
+                    "\"name\" : \"CSEDAR\"," +
+                    "\"fieldOfStudy\" : \"Education Didactics and Applications Research\"," +
+                    "\"leader\" : \"Valteri.Bottas@gmail.com\"" +
+                    "}," +
+                    "{" +
+                    "\"name\" : \"Sexy-Girl\"," +
+                    "\"fieldOfStudy\" : \"Swag, dank memes and 360 noScopes\"," +
+                    "\"leader\" : \"mark\"" +
+                    "}" +
+                    "]";
+            Save(file, groupsJsonString);
+        }
 
-        String jsonString = "[ {\"name\" : \"CIRG\",\"fieldOfStudy\" : \"Artifical Inteligence\",\"leader\" : \"1\"}, {\"name\" : \"ICSA\",\"fieldOfStudy\" : \"Distributed Systems Security and Privacy\",\"leader\" : \"2\"}, {\"name\" : \"SSFM\",\"fieldOfStudy\" : \"System Specifications and Formal Methods\",\"leader\" : \"3\"},{\"name\" : \"SESAr\",\"fieldOfStudy\" : \"Software Engineering and Software Architecture\",\"leader\" : \"4\"}, {\"name\" : \"CSEDAR\",\"fieldOfStudy\" : \"Education Didactics and Applications Research\",\"leader\" : \"5\"}, {\"name\" : \"SexyGurls\",\"fieldOfStudy\" : \"Swag\",\"leader\" : \"Snoop Dawgg\"} ]";
-
+        //String jsonString = "[ {\"name\" : \"CIRG\",\"fieldOfStudy\" : \"Artifical Inteligence\",\"leader\" : \"1\"}, {\"name\" : \"ICSA\",\"fieldOfStudy\" : \"Distributed Systems Security and Privacy\",\"leader\" : \"2\"}, {\"name\" : \"SSFM\",\"fieldOfStudy\" : \"System Specifications and Formal Methods\",\"leader\" : \"3\"},{\"name\" : \"SESAr\",\"fieldOfStudy\" : \"Software Engineering and Software Architecture\",\"leader\" : \"4\"}, {\"name\" : \"CSEDAR\",\"fieldOfStudy\" : \"Education Didactics and Applications Research\",\"leader\" : \"5\"}, {\"name\" : \"SexyGurls\",\"fieldOfStudy\" : \"Swag\",\"leader\" : \"Snoop Dawgg\"} ]";
+        String jsonString = Load(file) ;
         try {
             JSONArray jsonArray = new JSONArray(jsonString);
             items = new ArrayList<Group>();
@@ -357,4 +403,90 @@ public class GroupList extends AppCompatActivity
         TextView group_name;
         ImageButton view_btn;
     }
+
+    public static String Load(File file)
+    {
+        FileInputStream fis = null;
+        try
+        {
+            fis = new FileInputStream(file);
+        }
+        catch (FileNotFoundException e) {e.printStackTrace();}
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        String test;
+        int anzahl=0;
+        try
+        {
+            while ((test=br.readLine()) != null)
+            {
+                anzahl++;
+            }
+        }
+        catch (IOException e) {e.printStackTrace();}
+
+        try
+        {
+            fis.getChannel().position(0);
+        }
+        catch (IOException e) {e.printStackTrace();}
+
+        String[] array = new String[anzahl];
+
+        String line;
+        int i = 0;
+        try
+        {
+            while((line=br.readLine())!=null)
+            {
+                array[i] = line;
+                i++;
+            }
+        }
+        catch (IOException e) {e.printStackTrace();}
+
+        String returnString = "";
+        for (int k = 0; k < array.length; k++)
+        {
+            returnString += array[k];
+        }
+
+        return returnString;
+    }
+    public static void Save(File file, String dataString)
+    {
+        String [] data = String.valueOf(dataString).split(System.getProperty("line.separator"));
+        FileOutputStream fos = null;
+        try
+        {
+            fos = new FileOutputStream(file);
+        }
+        catch (FileNotFoundException e) {e.printStackTrace();}
+        try
+        {
+            try
+            {
+                for (int i = 0; i<data.length; i++)
+                {
+                    fos.write(data[i].getBytes());
+                    if (i < data.length-1)
+                    {
+                        fos.write("\n".getBytes());
+                    }
+                }
+            }
+            catch (IOException e) {e.printStackTrace();}
+        }
+        finally
+        {
+            try
+            {
+                fos.close();
+            }
+            catch (IOException e) {e.printStackTrace();}
+        }
+    }
+
 }
+
